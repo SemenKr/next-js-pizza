587a33eedb698a37e8dd5b36d67ae59a61137bd6
semenkr <samkrekotyn@gmail.com>
2024-08-05 11:39:12 +0300

    добавляет кнопку карзины с иконками

--- src/components/shared/header.tsx
+++ src/components/shared/header.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { cn } from '@/lib/utils';
 import Image from 'next/image';
 import { Container } from './container';
+import { Button } from '../ui';
+import { ArrowRight, ShoppingCart, User } from 'lucide-react';
 
 interface Props {
 	className?: string;
@@ -9,7 +11,7 @@ interface Props {
 
 export const Header: React.FC<Props> = ({ className }) => {
 	return (
-		<header className={cn('border border-b', className)}>
+        <header className={cn('border border-b', className)}>
 			<Container className={'flex items-center justify-between py-8'}>
 				{/* Левая часть */}
 				<div className="flex items-center gap-4">
@@ -21,10 +23,25 @@ export const Header: React.FC<Props> = ({ className }) => {
 				</div>
 				{/* Правая часть */}
 				<div className="flex items-center gap-3">
+					<Button variant="outline" className="flex items-center gap-1">
+						<User size={16}/>
+						Войти
+					</Button>
 
+					<div className={'group relative'}>
+						<Button variant="default">
+							<b>520 ₽</b>		
+							   <span className="h-full w-[1px] bg-white/30 mx-3" />
+							<div className="flex items-center gap-1 transition duration-300 group-hover:opacity-0">
+							<ShoppingCart className="h-4 w-4 relative" strokeWidth={2} />
+							<b>3</b>
+       		  </div>
+						<ArrowRight className="w-5 absolute right-5 transition duration-300 -translate-x-2 opacity-0 group-hover:opacity-100 group-hover:translate-x-0" />
+						</Button>
 
+					</div>
          </div>
 			</Container>
 		</header>
-	);
+    );
 };
