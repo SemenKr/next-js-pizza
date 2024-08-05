772ad901b56a7dba7e1bf7d6696f2cd639324b5f
semenkr <samkrekotyn@gmail.com>
2024-08-05 12:58:15 +0300

    topBar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,12 @@
-import { Categories, Container, SortPopup, Title } from '@/components/shared';
+import { Categories, Container, SortPopup, Title, TopBar } from '@/components/shared';
 
 export default function Home() {
   return (
     <main>
 			<Container className='mt-10'>
 				<Title text='Все пиццы' size='lg' className='font-extrabold'/>
-
-				<Categories />
-				<SortPopup />
 			</Container>
+			<TopBar />
     </main>
   );
 }
