a704803b023d5e314cf5a04bda321ed1f28d8f13
semenkr <samkrekotyn@gmail.com>
2024-08-17 09:13:11 +0300

    фильтры

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import { Categories, Container, SortPopup, Title, TopBar } from '@/components/shared';
+import { Categories, Container, Filters, SortPopup, Title, TopBar } from '@/components/shared';
 
 export default function Home() {
   return (
@@ -7,6 +7,15 @@ export default function Home() {
 				<Title text='Все пиццы' size='lg' className='font-extrabold'/>
 			</Container>
 			<TopBar />
+
+			<Container className='pb-14'>
+				<div className='flex gap-[60px]'>
+					{/* Фильтрация */}
+					<div className="w-[250px]">
+						<Filters />
+					</div>
+				</div>
+			</Container>
     </main>
   );
 }
