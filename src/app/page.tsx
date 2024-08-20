b632886459f895c7f7fd5c5537aeeafca736f71e
semenkr <samkrekotyn@gmail.com>
2024-08-20 19:27:46 +0300

    feat добавляет список товаров на странницу

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import { Categories, Container, Filters, SortPopup, Title, TopBar } from '@/components/shared';
+import { ProductCard } from '@/components/shared/product-card';
 
 export default function Home() {
   return (
@@ -13,6 +14,13 @@ export default function Home() {
 					{/* Фильтрация */}
 					<div className="w-[250px]">
 						<Filters />
+					</div>
+
+					{/* Список товаров */}
+					<div className="flex-1">
+						<div className="flex flex-col gap-16">
+							<ProductCard id={0} name={'Чизбургер-пицца'} price={550} imageUrl={'https://media.dodostatic.net/image/r:584x584/11EE7D61706D472F9A5D71EB94149304.avif'} />
+						</div>
 					</div>
 				</div>
 			</Container>
