2aefcfed8661667dcfb8e1d0a54714ef70863587
Семён Крекотун <97591773+SemenKr@users.noreply.github.com>
2024-09-01 12:53:29 +0300

    роутинг продуктов

---
+++ src/app/api/products/search/route.ts
@@ -0,0 +1,20 @@
+import { NextRequest, NextResponse } from 'next/server';
+import { prisma } from '../../../../../prisma/prisma-client';
+
+export async function GET(request: NextRequest) { // типизация NextRequest
+
+	const query = request.nextUrl.searchParams.get('query') || '' // получаем параметр query из URL
+
+	const products = await prisma.product.findMany({
+		where: {
+			name: {
+				contains: query,
+				mode: 'insensitive',
+			}
+		},
+		take: 5,
+	})
+
+	return NextResponse.json({ products })
+
+}
