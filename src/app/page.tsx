7dd202ea8aff1ddec8594b200698b35e2ed1217b
Семён Крекотун <97591773+SemenKr@users.noreply.github.com>
2024-09-04 21:26:55 +0300

    добавляем категории на страницу из запроса на сервер

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,145 +1,51 @@
-import { Categories, Container, Filters, SortPopup, Title, TopBar } from '@/components/shared';
-import { ProductCard } from '@/components/shared/product-card';
-import { ProductsGroupList } from '@/components/shared/products-group-list';
+import {Container, Filters, Title, TopBar} from '@/components/shared';
+import {ProductsGroupList} from '@/components/shared/products-group-list';
+import {prisma} from "../../prisma/prisma-client";
+import React from "react";
 
-export default function Home() {
-  return (
-    <main>
-			<Container className='mt-10 pb-14'>
-				<Title text='Все пиццы' size='lg' className='font-extrabold'/>
-			</Container>
-			<TopBar />
+export default async function Home() {
 
-			<Container className='mt-10 pb-14'>
-				<div className='flex gap-[80px]'>
-					{/* Фильтрация */}
-					<div className="w-[250px]">
-						<Filters />
-					</div>
+    const categories = await prisma.category.findMany({
+        include: {
+            products: {
+                include: {
+                    ingredients: true,
+                    items: true
+                }
+            }
+        }
+    });
 
-					{/* Список товаров */}
-					<div className="flex-1">
-						<div className="flex flex-col gap-16">
-							<ProductsGroupList title={'Пиццы'} items={[
-								{
-									id: 1,
-									name: 'Чоризо фреш',
-									imageUrl: 'https://media.dodostatic.net/image/r:584x584/11EE7D61706D472F9A5D71EB94149304.avif',
-									price: 300,
-									items: [{price: 300}]
-								},
-								{
-									id: 2,
-									name: 'Чоризо фреш',
-									imageUrl: 'https://media.dodostatic.net/image/r:584x584/11EE7D61706D472F9A5D71EB94149304.avif',
-									price: 300,
-									items: [{price: 300}]
-								},
-								{
-									id: 3,
-									name: 'Чоризо фреш',
-									imageUrl: 'https://media.dodostatic.net/image/r:584x584/11EE7D61706D472F9A5D71EB94149304.avif',
-									price: 300,
-									items: [{price: 300}]
-								},
-								{
-									id: 4,
-									name: 'Чоризо фреш',
-									imageUrl: 'https://media.dodostatic.net/image/r:584x584/11EE7D61706D472F9A5D71EB94149304.avif',
-									price: 300,
-									items: [{price: 300}]
-								},
-								{
-									id: 5,
-									name: 'Чоризо фреш',
-									imageUrl: 'https://media.dodostatic.net/image/r:584x584/11EE7D61706D472F9A5D71EB94149304.avif',
-									price: 300,
-									items: [{price: 300}]
-								},
-								{
-									id: 6,
-									name: 'Чоризо фреш',
-									imageUrl: 'https://media.dodostatic.net/image/r:584x584/11EE7D61706D472F9A5D71EB94149304.avif',
-									price: 300,
-									items: [{price: 300}]
-								},
-								{
-									id: 7,
-									name: 'Чоризо фреш',
-									imageUrl: 'https://media.dodostatic.net/image/r:584x584/11EE7D61706D472F9A5D71EB94149304.avif',
-									price: 300,
-									items: [{price: 300}]
-								},
-								{
-									id: 8,
-									name: 'Чоризо фреш',
-									imageUrl: 'https://media.dodostatic.net/image/r:584x584/11EE7D61706D472F9A5D71EB94149304.avif',
-									price: 300,
-									items: [{price: 300}]
-								},
-							]} categoryId={1} 	/>
-							<ProductsGroupList title={'Комбо'} items={[
-									{
-										id: 1,
-										name: 'Чоризо фреш',
-										imageUrl: 'https://media.dodostatic.net/image/r:584x584/11EE7D61706D472F9A5D71EB94149304.avif',
-										price: 300,
-										items: [{price: 300}]
-									},
-									{
-										id: 2,
-										name: 'Чоризо фреш',
-										imageUrl: 'https://media.dodostatic.net/image/r:584x584/11EE7D61706D472F9A5D71EB94149304.avif',
-										price: 300,
-										items: [{price: 300}]
-									},
-									{
-										id: 3,
-										name: 'Чоризо фреш',
-										imageUrl: 'https://media.dodostatic.net/image/r:584x584/11EE7D61706D472F9A5D71EB94149304.avif',
-										price: 300,
-										items: [{price: 300}]
-									},
-									{
-										id: 4,
-										name: 'Чоризо фреш',
-										imageUrl: 'https://media.dodostatic.net/image/r:584x584/11EE7D61706D472F9A5D71EB94149304.avif',
-										price: 300,
-										items: [{price: 300}]
-									},
-									{
-										id: 5,
-										name: 'Чоризо фреш',
-										imageUrl: 'https://media.dodostatic.net/image/r:584x584/11EE7D61706D472F9A5D71EB94149304.avif',
-										price: 300,
-										items: [{price: 300}]
-									},
-									{
-										id: 6,
-										name: 'Чоризо фреш',
-										imageUrl: 'https://media.dodostatic.net/image/r:584x584/11EE7D61706D472F9A5D71EB94149304.avif',
-										price: 300,
-										items: [{price: 300}]
-									},
-									{
-										id: 7,
-										name: 'Чоризо фреш',
-										imageUrl: 'https://media.dodostatic.net/image/r:584x584/11EE7D61706D472F9A5D71EB94149304.avif',
-										price: 300,
-										items: [{price: 300}]
-									},
-									{
-										id: 8,
-										name: 'Чоризо фреш',
-										imageUrl: 'https://media.dodostatic.net/image/r:584x584/11EE7D61706D472F9A5D71EB94149304.avif',
-										price: 300,
-										items: [{price: 300}]
-									},
-							]} categoryId={2} 	/>
-						</div>
-					</div>
-				</div>
-			</Container>
-    </main>
-  );
+    return (
+        <main>
+            <Container className='mt-10 pb-14'>
+                <Title text='Все пиццы' size='lg' className='font-extrabold'/>
+            </Container>
+            <TopBar categories={categories} />
+
+            <Container className='mt-10 pb-14'>
+                <div className='flex gap-[80px]'>
+                    {/* Фильтрация */}
+                    <div className="w-[250px]">
+                        <Filters/>
+                    </div>
+
+                    {/* Список товаров */}
+                    <div className="flex-1">
+                        <div className="flex flex-col gap-16">
+                            {categories.map((category) => (
+                                category.products.length > 0 && (
+                                    <ProductsGroupList
+                                        key={category.id}
+                                        title={category.name}
+                                        items={category.products}
+                                        categoryId={category.id}/>
+                                )
+                            ))}
+                        </div>
+                    </div>
+                </div>
+            </Container>
+        </main>
+    );
 }
--- src/components/shared/categories.tsx
+++ src/components/shared/categories.tsx
@@ -3,9 +3,11 @@
 import React from 'react';
 import { cn } from '@/lib/utils';
 import { useCategoryStore } from '../../../store/category';
+import {Category} from "@prisma/client";
 
 interface Props {
 	className?: string;
+	items: Category[];
 }
 
 const categories = [
@@ -17,17 +19,17 @@ const categories = [
 	{ id: 6, name: 'Закрытые' },
 ];
 
-export const Categories: React.FC<Props> = ({ className }) => {
+export const Categories: React.FC<Props> = ({items, className }) => {
 
 const categoryActiveId = useCategoryStore((state) => state.activeId);
 
 	return (
 		<div className={cn('inline-flex gap-1 bg-gray-50  p-1 rounded-2xl ', className)}>
-			{categories.map(({name , id}, index) => (
+			{items.map(({name , id}, index) => (
 				<a className={cn(
 					'flex items-center h-11 rounded-2xl px-5 font-bold',
 					categoryActiveId === id && 'bg-white shadow-md shadow-gray-200 text-primary',
-				)} 
+				)}
 				key={index}
 				href={`#${name}`}
 				>
