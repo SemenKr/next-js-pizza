87097dd8d298b50ba98757f44fc1f77efc9b93a8
Семён Крекотун <97591773+SemenKr@users.noreply.github.com>
2024-12-15 16:41:03 +0300

    update route.ts fix import prisma

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -1,5 +1,5 @@
+import { prisma } from '@/prisma/prisma-client';
 import { NextRequest, NextResponse } from 'next/server'
-import { prisma } from '../../../../prisma/prisma-client'
 
 export const GET = async () => {
 	const users = await prisma.user.findMany()
